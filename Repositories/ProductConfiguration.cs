7ee7b47f2833813f10360f9ea570c6575acdc40b
Yusuf Deniz <yd.yusufdeniz41@gmail.com>
2025-07-04 00:39:45 +0300

    B2.4 Repositories(DbContext - Entity)

---
+++ Repositories/ProductConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace App.Repositories
+{
+    public class ProductConfiguration : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.Property(x=>x.Name).IsRequired();
+            builder.Property(x => x.Description).IsRequired();
+            builder.Property(x => x.Price).HasColumnType("decimal(18,2)");
+            builder.Property(x=>x.Stock).IsRequired();
+        }
+    }
+}
